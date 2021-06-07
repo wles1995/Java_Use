a266558fb9ff4c8ecde4245fe2bd8368576477e6
wles1995 <58210069+wles1995@users.noreply.github.com>
2021-06-07 10:00:26 +0800

    Update Hello.java
    fifth

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,6 @@ public class Hello {
         System.out.println("first commit");
         System.out.println("second commit");
         System.out.println("third commit");
-
+        System.out.println("forth commit");
     }
 }
