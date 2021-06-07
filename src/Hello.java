15860f728116f190495a2313686818ae3982f6eb
wles <435829953@qq.com>
2021-06-07 09:34:07 +0800

    second_commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
+        System.out.println("first commit");
+
     }
 }
